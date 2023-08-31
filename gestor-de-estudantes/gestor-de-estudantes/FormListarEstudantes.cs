297fdcfa054196596dda1ec3cac705acd69de623
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-08-31 18:08:11 -0300

    Listar Estudantes

--- gestor-de-estudantes/gestor-de-estudantes/FormListarEstudantes.cs
+++ gestor-de-estudantes/gestor-de-estudantes/FormListarEstudantes.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -39,6 +40,28 @@ namespace gestor_de_estudantes
 
         private void dataGridViewLista_DoubleClick(object sender, EventArgs e)
         {
+            FormAtualizarEstudantes atualizarEstudante = new FormAtualizarEstudantes();
+            atualizarEstudante.textBoxId.Text = dataGridViewLista.CurrentRow.Cells[0].Value.ToString();
+            atualizarEstudante.textBoxNome.Text= dataGridViewLista.CurrentRow.Cells[1].Value.ToString();
+            atualizarEstudante.textBoxSobrenome.Text = dataGridViewLista.CurrentRow.Cells[2].Value.ToString();
+            atualizarEstudante.dateTimePickerNascimento.Value = (DateTime) dataGridViewLista.CurrentRow.Cells[3].Value;
+
+            if (dataGridViewLista.CurrentRow.Cells[4].Value.ToString() == "Feminino")
+            {
+                atualizarEstudante.radioButtonFeminino.Checked = true;
+            } else
+            {
+                atualizarEstudante.radioButtonMasculino.Checked = true;
+            }
+
+            atualizarEstudante.textBoxTelefone.Text = dataGridViewLista.CurrentRow.Cells[5].Value.ToString();
+            atualizarEstudante.textBoxEndereco.Text = dataGridViewLista.CurrentRow.Cells[6].Value.ToString();
+
+            byte[] fotoDaLista;
+            fotoDaLista = (byte[]) dataGridViewLista.CurrentRow.Cells[7].Value;
+            MemoryStream fotoDoEstudante = new MemoryStream(fotoDaLista);
+            atualizarEstudante.pictureBoxFoto.Image = Image.FromStream(fotoDoEstudante);
+            atualizarEstudante.Show();
 
         }
     }
